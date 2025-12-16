efd2e4d17b372c116c25e59f188fa0aee2ec7df6
simarpreet <simarkaur6011@gmail.com>
2025-12-16 23:58:13 +0530

    Basic program

---
+++ Module1/BasicProgram.java
@@ -0,0 +1,9 @@
+public class BasicProgram {
+       public static void main(String args[]){
+        System.out.print("Hi ".repeat(3)); //same line
+        System.out.println("Simar");  //new line
+        System.out.println("How are you?");
+
+}
+    
+}
