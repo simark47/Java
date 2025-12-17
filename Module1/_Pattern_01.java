064dee069fcc4c9b39897cea555e1a052f6a43c4
simarpreet <simarkaur6011@gmail.com>
2025-12-17 18:24:44 +0530

    Basics

--- Module1/_Pattern_01.java
+++ Module1/_Pattern_01.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // * * *  
 // * * * 
 // * * * 
-public class Pattern_01 {
+public class _Pattern_01 {
     public static void main(String args[]) {
 
   Scanner sc = new Scanner(System.in);
